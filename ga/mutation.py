e855a48d5faf0e469741ffdd24f3e51e265d19ed
Edmond <mredmondwang@gmail.com>
2025-01-01 06:55:29 +0800

    Rewrite mutation function to be more similiar to the EUPA paper
    Make mutation more rare, less random, to focus more on the pixel cleaning operation. Before a lot of pixels get cleaned but then randomly mutated so the two operations sort of cancel out.

--- ga/mutation.py
+++ ga/mutation.py
@@ -2,6 +2,27 @@ import numpy as np
 import torch
 from ga.fitness import apply_pixel_constraints
 
+def paper_style_mutation(offspring, ga_instance, p_m=0.3, p_flip=0.005):
+    """
+    - Pick out a fraction p_m of solutions for mutation
+    - For each solution, each gene has a small probability (p_flip) to be re-randomized
+    """
+    population_size = len(offspring)
+
+    # 1 - pick a sub-population for mutation
+    num_mutation_chromosomes = int(np.ceil(p_m * population_size))
+    mutation_ind = np.random.choice(population_size, num_mutation_chromosomes, replace=False)
+
+    # 2 - for each chromosome in the sub-population, flip each gene with a small probability
+    for i in mutation_ind:
+        chromosome = offspring[i]
+        for gene_idx in range(len(chromosome)):
+            if np.random.rand() < p_flip:
+                chromosome[gene_idx] = np.random.uniform(ga_instance.init_range_low, ga_instance.init_range_high)
+
+    return offspring
+
+
 def random_float_mutation(offspring, ga_instance):
 
     # # Apply pixel cleaning operation before mutation
